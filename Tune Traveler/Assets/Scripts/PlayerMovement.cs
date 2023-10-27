aa174f4620b8945abfb434c57906353b769d14ab
EyeballSweat <eyeballsweatgaming@gmail.com>
2023-10-27 18:24:23 +0100

    Jump Tinkering
    Made it so James can't infinitely jump in the air and can't cling to walls. James has 2 jumps if starting from the ground, but only one if he is starting from falling.

--- Tune Traveler/Assets/Scripts/PlayerMovement.cs
+++ Tune Traveler/Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
     private SpriteRenderer sprite;
     private Animator anim;
+
+    [SerializeField] private LayerMask jumpableGround;
+    [SerializeField] private int maxJumps = 2;
+    private int jumpsLeft;
 
     private float dirX = 0f;
    [SerializeField] private float moveSpeed = 7f;
    [SerializeField] private float jumpForce = 10f;
 
+    private enum MovementState { idle, walking, jumping, falling }
+
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        jumpsLeft = maxJumps;
     }
 
     // Update is called once per frame
@@ -26,9 +35,19 @@ public class PlayerMovement : MonoBehaviour
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (IsGrounded() && rb.velocity.y <= 0f)
+        {
+            jumpsLeft = maxJumps;
+        }
+        else if ((IsGrounded() == false) && jumpsLeft > 1)
+        {
+            jumpsLeft = 1;
+        }
+
+        if (Input.GetButtonDown("Jump") && jumpsLeft > 0)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpsLeft -= 1;
         }
 
         UpdateAnimationState();
@@ -36,19 +55,37 @@ public class PlayerMovement : MonoBehaviour
 
     private void UpdateAnimationState()
     {
+        MovementState moveState;
+
         if (dirX > 0f)
         {
-            anim.SetBool("Walking", true);
+            moveState = MovementState.walking;
             sprite.flipX = false;
         }
         else if (dirX < 0f)
         {
-            anim.SetBool("Walking", true);
+            moveState = MovementState.walking;
             sprite.flipX = true;
         }
         else
         {
-            anim.SetBool("Walking", false);
+            moveState = MovementState.idle;
         }
+
+        if (rb.velocity.y > .1f)
+        {
+            moveState = MovementState.jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            moveState = MovementState.falling;
+        }
+
+        anim.SetInteger("MovementState", (int)moveState);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
