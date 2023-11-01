5680ca3eea295b622a54c0701bf9d0b6f90990c9
EyeballSweat <eyeballsweatgaming@gmail.com>
2023-11-01 00:27:37 +0000

    Hearts + Misc [READ DESC]
    HeartBar script has an error. Can't figure it out for the life of me so far but I'm trying. Still pushed because a lot of other things have changed too.
    
    Co-Authored-By: RepulsiveH <149063736+RepulsiveH@users.noreply.github.com>

--- Tune Traveler/Assets/Scripts/PlayerLife.cs
+++ Tune Traveler/Assets/Scripts/PlayerLife.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Schema;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.U2D.Animation;
 
 public class PlayerLife : MonoBehaviour
 {
@@ -9,19 +11,37 @@ public class PlayerLife : MonoBehaviour
     private Animator anim;
 
     [SerializeField] private AudioSource deathSoundEffect;
+    [SerializeField] private AudioSource hurtSoundEffect;
+    public int playerHealth = 0;
+    public int maxHealth = 6;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        playerHealth = maxHealth;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       if (collision.gameObject.CompareTag("Hostile"))
-       {
-            Die();
-       }
+        if (collision.gameObject.CompareTag("Hostile"))
+        {
+            if (playerHealth == 1)
+            {
+                playerHealth = playerHealth - 1;
+                Debug.Log("Player Died");
+                Die();
+            }
+            playerHealth = playerHealth - 1;
+            Debug.Log("Health Left: " + playerHealth);
+            Hurt();
+        }
+    }
+
+    private void Hurt()
+    {
+        hurtSoundEffect.Play();
+        anim.SetTrigger("Hurt");
     }
 
     private void Die()
